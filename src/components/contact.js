a672606b2b602a13fe9cccb21aa2897ded721898
Joshua Grossman <Josh1794@gmail.com>
2020-04-29 16:01:32 -0400

    contact styling change

--- src/components/contact.js
+++ src/components/contact.js
@@ -23,33 +23,34 @@ export default function Contact() {
     <div id="contactPage" class="contactPage">
       <br />
       <br />
-
-      <h2>Contact Details</h2>
-      <div class="contactInfo">
-        <p className="address">
-          <h3>Name:</h3>
-          <h4>{name}</h4>
+      <div>
+        <h2>Contact Details</h2>
+        <div class="contactInfo">
+          <p className="address">
+            <h3>Name:</h3>
+            <h4>{name}</h4>
+            <br />
+            <h3>Phone:</h3>
+            <h4>{phone}</h4>
+            <br />
+            <h3>Email:</h3>
+            <a class="emailLink" href="mailto:josh1794@gmail.com">
+              {email}
+            </a>
+          </p>
           <br />
-          <h3>Phone:</h3>
-          <h4>{phone}</h4>
-          <br />
-          <h3>Email:</h3>
-          <a class="emailLink" href="mailto:josh1794@gmail.com">
-            {email}
-          </a>
-        </p>
-        <br />
-        <p>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            class="resumeButton"
-            href="https://drive.google.com/file/d/1YtYsYM6FEHskBKUzoZZK8_qgTt1q-NzJ/view?usp=sharing"
-            aria-label="My Resume "
-          >
-            My Resume <i class="fas fa-download"></i>
-          </a>
-        </p>
+          <p>
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              class="resumeButton"
+              href="https://drive.google.com/file/d/1YtYsYM6FEHskBKUzoZZK8_qgTt1q-NzJ/view?usp=sharing"
+              aria-label="My Resume "
+            >
+              My Resume <i class="fas fa-download"></i>
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   )
