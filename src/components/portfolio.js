89d20226a3149a12b789abbb1bda07571f08e90f
Joshua Grossman <Josh1794@gmail.com>
2020-04-29 14:43:28 -0400

    made portfolio projects open in a new tab/window

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -15,28 +15,47 @@ const Portfolio = () => (
         <h3 align="center">Check out some of my projects</h3>
         <div class="portfolioProjects">
           <a
+            target="_blank"
             class="projects"
             href="https://github.com/thunky-monks/graceshopper"
+            rel="noreferrer noopener"
           >
             <Cart />
             <h6 class="portfolioSubTitle">E-Commerce-Commerce</h6>
           </a>
-          <a class="projects" href="https://github.com/Josh1794/Sensor-app">
+          <a
+            target="_blank"
+            class="projects"
+            href="https://github.com/Josh1794/Sensor-app"
+            rel="noreferrer noopener"
+          >
             <Sensor />
             <h6 class="portfolioSubTitle">Sensors App</h6>
           </a>
           <a
+            target="_blank"
             class="projects"
             href="https://github.com/Thunkaroos/YellowBrickRoad"
+            rel="noreferrer noopener"
           >
             <YBR />
             <h6 class="portfolioSubTitle">Yellow Brick Road</h6>
           </a>
-          <a class="projects" href="https://github.com/Josh1794/myWeatherApp">
+          <a
+            target="_blank"
+            class="projects"
+            href="https://github.com/Josh1794/myWeatherApp"
+            rel="noreferrer noopener"
+          >
             <Weather />
             <h6 class="portfolioSubTitle">Weather App</h6>
           </a>
-          <a class="projects" href="https://github.com/Josh1794/recipeBook">
+          <a
+            target="_blank"
+            class="projects"
+            href="https://github.com/Josh1794/recipeBook"
+            rel="noreferrer noopener"
+          >
             <Recipe />
             <h6 class="portfolioSubTitle">Recipe Book</h6>
           </a>
