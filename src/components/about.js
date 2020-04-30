2115c5a591a7bffe728b6aa32ba4d8356625cd46
Joshua Grossman <Josh1794@gmail.com>
2020-04-29 22:07:00 -0400

    cleaned up about component code

--- src/components/about.js
+++ src/components/about.js
@@ -22,29 +22,10 @@ export default function About() {
     <div id="aboutPage" class="aboutPage">
       <br />
       <br />
-
       <h2 class="aboutHeader">
         About me <Image />
       </h2>
       <body>{bio}</body>
-      {/* <br />
-
-      <body>{tech}</body>
-      <br />
-      <ul>
-        <li>JavaScript (ES6+)</li>
-        <li>ReactJS</li>
-        <li>React-Native</li>
-        <li>Gatsby.js</li>
-        <li>Next.js</li>
-        <li>Redux.js</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>Sequelize.js</li>
-        <li>PostgreSQL</li>
-      </ul> */}
-      {/* <div data-sal="fade" id="aboutAnimation"> */}
-      {/* </div> */}
     </div>
   )
 }
